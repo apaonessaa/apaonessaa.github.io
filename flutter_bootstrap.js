4a59e4902a37a61376b4d953b101aaac5231820f
Antonio Paonessa <132266601+apaonessaa@users.noreply.github.com>
2025-08-09 11:40:17 +0200

    routes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "572052554"
+    serviceWorkerVersion: "3937151343"
   }
 });
