9472cf004b60f4abe826f4a34fc18bb2493b2e24
Antonio Paonessa <132266601+apaonessaa@users.noreply.github.com>
2025-10-26 20:12:35 +0100

    .

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4079198041"
+    serviceWorkerVersion: "1423166291"
   }
 });
